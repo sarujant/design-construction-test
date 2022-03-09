c54d69cd3d99585d9cb0671bbbd02dda1ae03c6b
sarujant <sarujan1234567@gmail.com>
2022-03-09 15:30:44 +0000

    update

--- Multimeter/Multimeter.c
+++ Multimeter/Multimeter.c
@@ -10,6 +10,9 @@ void SysTick_Handler(void);
 void EXTI0_IRQHandler(void);
 
 void initialise_adc(void);
+void timer_etr_setup(void);
+void dac_out(void);
+
 void dc_voltage(void);
 void ac_voltage(void);
 void current(void);
@@ -52,6 +55,7 @@ int main() {
 	
 	PB_LCD_Init();
 	initialise_adc();
+	dac_out();
 	
 	while(1) {
 		if (startup_screen_status == 1) {
@@ -77,43 +81,29 @@ int main() {
 void menu() {
 			switch(menu_select_counter) {
 			case 1 :
-				PB_LCD_Clear();
-				//char dcVoltage[50];
-				//int dcVLen = snprintf(dcVoltage, 50, "DC Voltage");
-				PB_LCD_GoToXY (0, 0);
-				PB_LCD_WriteString("---DC Voltage---", 50);
-				
-				//dc_voltage();
+				dc_voltage();
 				break;
 			case 2 :
-				PB_LCD_Clear();
-				//char acVoltage[50];
-				//int acVLen = snprintf(acVoltage, 50, "AC Voltage");
+				PB_LCD_Clear();			
 				PB_LCD_GoToXY (0, 0);
 				PB_LCD_WriteString("---AC Voltage---", 50);
-				//ac_voltage();
+				ac_voltage();
 				break;
 			case 3 :
-				PB_LCD_Clear();
-				//char current[50];
-				//int currLen = snprintf(current, 50, "Current");
+				PB_LCD_Clear();				
 				PB_LCD_GoToXY (0, 0);
 				PB_LCD_WriteString("----Current ----", 50);
-				//current();
+				current();
 				break;
 			case 4 :
 				menu_select_counter = 0;
-				PB_LCD_Clear();
-				//char resistance[50];
-				//int resLen = snprintf(resistance, 50, "Resistance");
+				PB_LCD_Clear();				
 				PB_LCD_GoToXY (0, 0);
 				PB_LCD_WriteString("---Resistance---", 50);				
-				//resistance();
+				resistance();
 				break;
 			default:
-				PB_LCD_Clear();
-				//char error[50];
-				//int errorLen = snprintf(error, 50, "Error lol");
+				PB_LCD_Clear();				
 				PB_LCD_GoToXY (0, 0);
 				PB_LCD_WriteString("---- ERROR! ----", 50);
 				PB_LCD_GoToXY (0, 1);
@@ -178,10 +168,39 @@ void EXTI0_IRQHandler(void) {
 }
 
 void dc_voltage(void) {
+	DAC->DHR12R1 = 1;
+	
+	PB_LCD_Clear();
+	PB_LCD_GoToXY (0, 0);
+	PB_LCD_WriteString("---DC Voltage---", 50);
+	
+	delay(STARTUP_SCREEN_DELAY);
+	
+	PB_LCD_Clear();
+	PB_LCD_GoToXY(0,0);
+	
+	// Okay but how are we gonna know which ADC to use before we know the output voltage as if we are using different ADCs depending on output, 
+	// 	we need to know the output of the ADC first to determine whether it's in the 10V range or 1V range to use either ADC1 or ADC2.
+	
+	ADC1->CR2 |= ADC_CR2_SWSTART_Msk; 
+	while(ADC1->SR != (ADC1->SR | ADC_SR_EOC_Msk)){}
+	adc1_conv = ADC1->DR;
+		
+	if(adc1_conv < 40.96) {
+		adc1_conv = 0;
+		ADC2->CR2 |= ADC_CR2_SWSTART_Msk; 
+		while(ADC2->SR != (ADC2->SR | ADC_SR_EOC_Msk)){} 
+		adc2_conv = ADC2->DR;	
+	}
+	
+	
+	 
+	
 	
 }
 
 void ac_voltage(void) {
+	DAC->DHR12R1 = 2;
 	ADC1->CR2 |= ADC_CR2_SWSTART_Msk; 
 	while(ADC1->SR != (ADC1->SR | ADC_SR_EOC_Msk)){} 
 	adc1_conv = ADC1->DR;
@@ -191,17 +210,25 @@ void ac_voltage(void) {
 }
 
 void current(void) {
+	DAC->DHR12R1 = 3;
 	ADC1->CR2 |= ADC_CR2_SWSTART_Msk; 
 	while(ADC1->SR != (ADC1->SR | ADC_SR_EOC_Msk)){} 
 	adc1_conv = ADC1->DR;
 }
 
 void resistance(void) {
+	DAC->DHR12R1 = 4;
 	ADC1->CR2 |= ADC_CR2_SWSTART_Msk; 
 	while(ADC1->SR != (ADC1->SR | ADC_SR_EOC_Msk)){} 
 	adc1_conv = ADC1->DR;
 }
 void initialise_adc(void) {
+	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
+	GPIOA->MODER |= GPIO_MODER_MODE1_Msk;
+	GPIOA->OTYPER &= ~GPIO_OTYPER_OT1_Msk;
+	GPIOA->OSPEEDR &= ~GPIO_OSPEEDR_OSPEED1_Msk;
+	GPIOA->PUPDR &= ~GPIO_PUPDR_PUPD1_Msk;
+	
 	RCC->APB2ENR |= RCC_APB2ENR_ADC1EN;
 	RCC->APB2ENR |= RCC_APB2ENR_ADC2EN;
 	
@@ -218,4 +245,28 @@ void initialise_adc(void) {
 	ADC2->CR2 |= ADC_CR2_ADON_Msk; 
 	ADC2->CR1 |= ADC_CR1_DISCEN_Msk;
 	ADC2->CR2 |= ADC_CR2_EOCS_Msk;
+}
+
+void timer_etr_setup(void){
+	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOEEN;
+	RCC->APB2ENR |= RCC_APB2ENR_TIM1EN;// Enables TIM1
+	
+	GPIOE->MODER &= ~GPIO_MODER_MODER7_Msk; //PE7 -> sets MODER to alternate function mode
+	GPIOE->MODER = 2 << GPIO_MODER_MODER7_Pos; 
+	GPIOE->PUPDR &= GPIO_PUPDR_PUPD7_Msk;
+	GPIOE->PUPDR = 2 << GPIO_PUPDR_PUPD7_Pos;
+	
+	GPIOE->AFR[0] &= GPIO_AFRL_AFSEL7_Msk; // Select PE7 for alt func low reg
+	GPIOE->AFR[0]  |= 1 << GPIO_AFRL_AFSEL7_Pos; // AF1 as that's what the alt func for TIM1_ETR is for PE7
+	
+	TIM1->SMCR |= TIM_SMCR_ECE;
+	TIM1->CR1 |= TIM_CR1_CEN;
+}
+
+void dac_out(void) {
+	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
+	GPIOA->MODER |= GPIO_MODER_MODE4_Msk; // (GPIOA->MODER & ~GPIO_MODER_MODER4_Msk) | (0x11 << GPIO_MODER_MODER4_Pos);
+	GPIOA->OTYPER &= ~GPIO_OTYPER_OT4_Msk;
+	GPIOA->OSPEEDR &= ~GPIO_OSPEEDR_OSPEED4_Msk;
+	GPIOA->PUPDR &= ~GPIO_PUPDR_PUPD4_Msk;
 }
